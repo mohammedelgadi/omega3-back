abbb320214481ce8a915ff05f7db3071e54a6d06
el gadi <mohelgadi@gmail.com>
2019-07-28 00:47:49 +0200

    First commit

---
+++ src/main/java/com/itdev/omega3back/Omega3BackApplication.java
@@ -0,0 +1,13 @@
+package com.itdev.omega3back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Omega3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Omega3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itdev/omega3back/Omega3BackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itdev.omega3back;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Omega3BackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
