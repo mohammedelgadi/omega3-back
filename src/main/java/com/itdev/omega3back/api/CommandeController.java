5bf4d92d80da5cf6cb512e241bed6b0d428cca01
mohammed el gadi <mohelgadi@gmail.com>
2019-07-28 20:52:40 +0200

    initialisation du service REST

--- src/main/java/com/itdev/omega3back/api/CommandeController.java
+++ src/main/java/com/itdev/omega3back/api/CommandeController.java
@@ -1,4 +1,20 @@
 package com.itdev.omega3back.api;
 
+import com.itdev.omega3back.model.Commande;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 public class CommandeController {
+    @GetMapping("/api/commandes")
+    public List<Commande> getCommandes(){
+        return Arrays.asList(
+                new Commande(1, "mohammed"),
+                new Commande(2, "achraf")
+        );
+    }
 }
---
+++ src/main/java/com/itdev/omega3back/model/Commande.java
@@ -0,0 +1,31 @@
+package com.itdev.omega3back.model;
+
+public class Commande {
+
+    private Integer id;
+
+    private String title;
+
+    public Commande(Integer id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
